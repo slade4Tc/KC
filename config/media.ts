1a9522a6c02059bc2826c87f835c39528ffeef8f
Slade4T <captainzeroseven@gmail.com>
2026-03-03 19:02:26 +0000

    NEXT_PUBLIC_R2_BASE_URL

--- config/media.ts
+++ config/media.ts
@@ -1,2 +1,6 @@
 export const IMAGE_BASE_URL =
-  (process.env.NEXT_PUBLIC_IMAGE_BASE_URL ?? 'https://pub-81e8ad22dc1f4213a0c54798e94af7b1.r2.dev').replace(/\/+$/, '');
+  (
+    process.env.NEXT_PUBLIC_R2_BASE_URL ??
+    process.env.NEXT_PUBLIC_IMAGE_BASE_URL ??
+    'https://pub-81e8ad22dc1f4213a0c54798e94af7b1.r2.dev'
+  ).replace(/\/+$/, '');
